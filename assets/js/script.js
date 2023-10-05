16eee06512265a85f5d680d14fbdd3eaf2a4f2c6
nicolasrivera23 <113543987+nicolasrivera23@users.noreply.github.com>
2023-10-05 20:20:30 -0300

    segunda clase

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,66 @@ function tipoDatos() {
   console.log(persona);
 
 }
+
+function may() {
+
+  //crear variable que aloje el texto del párrafo dónde se extraen los datos
+  let texto = document.getElementById("struno").innerHTML;
+  console.log(texto.toLowerCase());
+
+}
+
+function contar() {
+  let contar = document.getElementById("struno").innerHTML;
+  console.log(contar.length);
+}
+
+function repetir() {
+  let texto = document.getElementById("struno").innerHTML;
+  console.log(texto.repeat(3));
+}
+
+function reemplazar() {
+  let texto = document.getElementById("struno").innerHTML;
+  console.log(texto.replace("blablabla","Lorem"));
+}
+
+function parint() {
+
+  let texto = "15a";
+  console.log( parseInt(texto) + 15 );
+
+}
+
+function parFloat() {
+  let numero = "3.1416";
+  console.log( parseFloat(numero) + 15 );
+}
+
+function numb() {
+  let bool = false;
+  let nro = "10";
+  console.log(Number(bool));
+}
+
+function oper() {
+  let nro1 = 20;
+  let nro2 = 8;
+
+  let total = nro1 + nro2;
+  console.log("La suma de los dos nros es : " + total);
+
+  total = nro1 - nro2;
+  console.log("La resta de los dos nros es : " + total);
+
+  total = nro1 * nro2;
+  console.log("La multiplicación de los dos nros es : " + total);
+
+  total = nro1 / nro2;
+  console.log("La división de los dos nros es : " + total);
+
+  total = nro1 % nro2;
+  console.log("El resto/módulo de la división de los dos nros es : " + total);
+}
+
+
