c6e42fc60a9eac1d86ede4a8fede1390fd4b0383
nicolasrivera23 <113543987+nicolasrivera23@users.noreply.github.com>
2023-10-05 19:16:17 -0300

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+
+function preguntarNombre() {
+
+  //declaro una variable que almacena el nombre que el usuario escribe en el prompt
+  let nombre = prompt('Ingrese su nombre:');
+
+  //en el método alert ponemos por parámetro la variable nombre
+  alert(nombre);
+
+  //por último mostramos por consola el valor de la variable nombre
+  console.log(nombre);
+}
+
+function noHaceNada() {
+  var hola = 'Bienvenido a Js';
+  let numero = 10000;
+  const PI = 3.1416;
+  alert(nombre2);
+  alert(PI);
+  PI = 3.11;
+  alert(PI);
+}
+
+function tipoDatos() {
+  let cadena = 'Texto de Ejemplo';
+  let numero = 3.1541;
+  let esMayor = false;
+  let persona = {
+    nombre: 'James',
+    apellido: 'Gosling',
+    edad: 33
+  }
+
+  console.log(persona);
+
+}
