d156bc1498c17cf0ec11ef6f5eecb77dec178b13
Nicolás Elias Rivera <nicolas.rivera@bue.edu.ar>
2023-10-10 20:36:02 -0300

    update tercera clase

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,4 +96,62 @@ function oper() {
   console.log("El resto/módulo de la división de los dos nros es : " + total);
 }
 
+function opesig() {
+  let numero = 18;
+  numero += 10;
+  console.log(numero);
+}
 
+function compar() {
+  let mayor = 12 < 10;
+  console.log(mayor);
+
+  let mayorigual = 125 >= 124;
+  console.log(mayorigual);
+
+  let iguales = '10' == 10;
+  console.log("Son iguales 10 string que 10 numerico? : " + iguales);
+
+  let distintos = '10' != 10;
+  console.log("son distintos: " + distintos);
+
+  let variable = 10 === 10;
+  console.log("Hay igualdad estricta? " + variable);
+
+  let variable2 = '10' === 10;
+  console.log("Hay igualdad estricta? " + variable2);
+
+  let variable3 = 10 !== 'sarasa';
+  console.log("Hay inigualdad estricta? " + variable);
+
+}
+
+function incdec() {
+  let numero = 10;
+  console.log("numero++ de 10 : " + numero++);
+  let numero2 = 20;
+  console.log("++numero de 20 : " + ++numero2);
+}
+
+function logic() {
+  let nro1 = 12;
+  let nro2 = 15;
+  let nro3 = 22;
+
+  let resultado = (nro1 < 12) && (nro3 >= 20);
+  console.log("Conjunción && : " + resultado);
+
+  resultado = (nro1 < 12) || (nro1 >= 20);
+  console.log("Operador || : " + resultado);
+
+  console.log("Operador NOT ! : " + !resultado)
+
+}
+
+function cond() {
+  if(5 > 6) {
+    alert('Verdadero entro al bloque if');
+  } else {
+    alert('La condición no se cumplió');
+  }
+}
